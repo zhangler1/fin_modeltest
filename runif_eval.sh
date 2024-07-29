e7be3d93c5e8d1595ca4efc35a9ddf8cd6ab91b6
zhangle <892770342@qq.co>
2024-07-29 18:23:08 +0800

    first commited

--- runif_eval.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/home/llm/mambaforge/envs/py310-torch23-cu121/bin/python -m finDatasets.instruction_following_eval.evaluation_main \
---input_data="finDatasets/instruction_following_eval/data/input_data.jsonl" \
---input_response_data="finDatasets/instruction_following_eval/data/input_response_data_spark_lite_2024-07-26 14:27:13.jsonl" \
---output_dir="finDatasets/instruction_following_eval/data"
-exit 0
